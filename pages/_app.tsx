049cdb797fc5fbe020e7fc99439922134b54d76b
wesdra <wesdralima@gmail.com>
2023-03-04 14:59:14 -0300

    mudando projeto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,25 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import logoImg from "../public/assets/logo-idexadoc.png"
 
-import { ToastProvider } from 'react-toast-notifications';
-import React from 'react';
-import { Router } from 'next/router';
-import NProgress from "nprogress"
+import styles from '../styles/App.module.css'
 
-import "nprogress/nprogress.css";
+import Image from "next/image"
+import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  React.useEffect(() => {
-    const handleRouteStart = () => NProgress.start();
-    const handleRouteDone = () => NProgress.done();
-
-    Router.events.on("routeChangeStart", handleRouteStart);
-    Router.events.on("routeChangeComplete", handleRouteDone);
-    Router.events.on("routeChangeError", handleRouteDone);
-
-    return () => {
-      // Make sure to remove the event handler on unmount!
-      Router.events.off("routeChangeStart", handleRouteStart);
-      Router.events.off("routeChangeComplete", handleRouteDone);
-      Router.events.off("routeChangeError", handleRouteDone);
-    };
-  }, []);
-  /* ... */
-
-
-  return (<>
-    {/* <NextNProgress /> */}
-    <ToastProvider placement="bottom-center">
-      <Component {...pageProps} />
-    </ToastProvider>
-    
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.bg}></div>
+        <div className={styles.header}>
+          <a className={styles.logo} href="https://bit.ly/3J9XjZC">
+            <Image src={logoImg.src} width={150} height={24} alt="clickable image" />
+          </ a>
+        </div>
+        <Component {...pageProps} />
+      </div>
     </>
-    )
+  )
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,148 +1,42 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import whatsapp from "../public/assets/whatsapp.svg"
+import duxgp from "../public/assets/duxgp.svg"
 
-import { Contatos } from '../components/contatos'
-import { Rodape } from '../components/rodape'
-import dynamic from 'next/dynamic'
-import { GoogleAnalytics } from '../components/googleanalytics'
-import Pixel from '../components/pixel/facebook'
+export default function Home() {
+  return (
 
-const DynamicVendedores = dynamic(
-    () => import('../components/vendedores'),
-    { loading: () => <span >carregando...</span> }
-)
+    //   <Head>
+    //     <title>Create Next App</title>
+    //     <meta name="description" content="Indexa Doc - Aplicativo para cartórios" />
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
 
-const Home: NextPage = () => {
-
-
-
-
-    // const [valor, setValor] = useState('')
-
-    // function gravarVendedores(event: FormEvent) {
-    //     event.preventDefault()
-
-    //    const ref = database.ref('cupons')
-    //     const cupomcurent = [{
-    //         "DataCadastro": "2022\/3\/29",
-    //         "cupom": "WES123",
-    //         "email": "wesdralima@gmail.com",
-    //         "id": "2255",
-    //         "municipio": "COTIA",
-    //         "nome": "Wesdra",
-    //         "regatado":false,
-    //         "telefone": "5511940413848",
-    //         "acesso": 0
-    //       },
-    //       {
-    //         "DataCadastro": "2022\/3\/29",
-    //         "cupom": "FER13213",
-    //         "email": "Fernanda@gmail.com",
-    //         "id": "2256",
-    //         "municipio": "COTIA",
-    //         "nome": "Fernanda",
-    //         "regatado":false,
-    //         "telefone": "5511992505764",
-    //         "acesso": 0
-    //       },
-    //       {
-    //         "DataCadastro": "2022\/3\/29",
-    //         "cupom": "TA364654",
-    //         "email": "talita@gmail.com",
-    //         "id": "2257",
-    //         "municipio": "COTIA",
-    //         "nome": "Talita",
-    //         "regatado":false,
-    //         "telefone": "5511992057241",
-    //         "acesso": 0
-    //       },
-    //     ]
-
-    //     { cupomcurent.map(cupons => (ref.push(cupons))) }
-
-
-    // const ref = database.ref('vendedores')
-    // const vendedores = [
-
-    //     { nome: 'Fernanda', img: '/fernanda.svg', online: true, link: 'https://vendas.megaoutletsofa.com.br/39TYFVO', id: '39TYFVO' },
-    //     { nome: 'Gustavo', img: '/fernanda.svg', online: true, link: 'https://vendas.megaoutletsofa.com.br/37zzbPU', id: '37zzbPU' },
-    //    // { nome: 'Ednaldo', img: '/ednaldo.svg', online: true, link: 'https://vendas.megaoutletsofa.com.br/39WBASx', id: '39WBASx' },
-    //     { nome: 'Taina', img: '/taina.svg', online: true, link: 'https://vendas.megaoutletsofa.com.br/2Y6DHkt', id: '2Y6DHkt' },
-    //     { nome: 'Aline', img: '/aline.svg', online: true, link: 'https://vendas.megaoutletsofa.com.br/3vMpGVq', id: '3vMpGVq' },
-    //    // { nome: 'Marta', img: '/marta.svg', online: true, link: 'https://vendas.megaoutletsofa.com.br/3BOTmCJ', id: '3BOTmCJ' },
-    //    // { nome: 'Monise', img: '/monise.svg', online: false, link: 'https://vendas.megaoutletsofa.com.br/3onMfOk', id: '3onMfOk' },
-
-    // ];
-
-    // { vendedores.map(vendedor => (ref.push(vendedor))) }
-
-
-    //     setValor('')
-    // }
-
-    // function atualizar(){
-    //     const ref = database.ref('cupons')
-
-    //     const dados = {
-    //         "id":"9052",
-    //         "telefone":"5511947272293",
-    //         "nome":"Eelisabeth",
-    //         "email":null,
-    //         "municipio":"SAO PAULO",
-    //         "DataCadastro":"2020\/9\/17",
-    //         "cupom":"23381174",
-    //         "regatado":"false",
-    //         "dtregatado":null
-    //         }
-
-    //         ref.child('chave').update(dados)
-
-
-    // }
-
-
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Mega Outlet - Vendas</title>
-                <meta name="description" content="Mega Outlet - Vendas" />
-                <meta property="og:title" content="Mega Outlet" />
-                <meta property="og:description" content="Mega Outlet - Loja de móveis: sofás, mesas, cadeiras, polronas, colchões, sofá cama, mesa de cabeceiras..." />
-                <meta property="og:url" content="https://vendas.megaoutletsofa.com.br/" />
-                <meta property="og:image" content="https://vendas.megaoutletsofa.com.br/loja.png" />
-                <meta property="og:type" content="website" />
-                <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;1,400&display=swap" rel="stylesheet" />
-                <link rel="icon" href="/favicon.ico" />
-                <Pixel scriptfbq="fbq('track', 'PaginaVendedores');" />
-            </Head>
-
-            <main className={styles.main}>
-                <GoogleAnalytics />
-
-                {/* <form onSubmit={gravarVendedores}>
-                    <input type="text" name={valor} onChange={(event => setValor(event.target.value))} />
-                    <button type='submit'> enviar</button>
-                </form> */}
-<img src="/logo.svg" alt="mega Outlet" className={styles.logo} />
-                {/* <img src="/outubrorosa.png" alt="mega Outlet" className={styles.logo} /> */}
-                {/* <img src="/novembroazulcancer.png" alt="mega Outlet" width={400} /> */}
-                {/* <h1 className={styles.title}>seja bem-vindo!</h1> */}
-                <br />
-                <small  >Fale com nossos consultores de venda pelo WhatsApp</small> <br />
-
-
-                <DynamicVendedores />
-
-
-
-                <Contatos />
-            </main>
-            <footer className={styles.footer}>
-                <Rodape />
-            </footer>
+      <main className={styles.main}>
+        <div className={styles.textwrap}>
+          <h1 className={styles.title}>
+            Transforma <span className={styles.textspan1}>cartórios&nbsp;comuns</span><br />
+            em <span className={styles.textspan2}>cartório digitais.</span>
+          </h1>
+          <p className={styles.textdesc}>Eficiencia, segurança e rapides.</p>
         </div>
-    )
-}
 
-export default Home
+
+        <Link href="https://bit.ly/3J9XjZC" className={styles.btnAgendamento}>
+            <div className={styles.btnBox}>
+            <Image src={whatsapp} className={styles.whats}  alt="Agende uma apresentação" />
+            <span>Agende uma apresentação</span>
+            </div>
+        </Link>
+
+        <div className="duxgp">
+          <Link href="https://duxgp.com.br/" passHref className="duxgplogo whats">
+           <Image src={duxgp} alt="DuxGP" />
+          </Link>
+        </div>
+
+      </main>
+  )
+}
