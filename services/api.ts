4470a0b22db2836203b00817e2c3204d29d075f8
wesdra <wesdralima@gmail.com>
2023-05-27 08:12:44 -0300

    api ebook url new

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://expertu.com.br/api/',
+    baseURL: 'https://mkt.indexadoc.com.br/api/',
     headers: {
       Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImVxdWlwZSIsImlhdCI6MTUxNjIzOTAyMn0.7g6E13Hk4PC2Jp-vkuUBJmVJR1A6al4q6hRrgEHFfLE`
     }
@@ -20,7 +20,7 @@ export const api = axios.create({
 // });
 
 export const apibase = axios.create({
-  baseURL: 'https://expertu.com.br/api/',
+  baseURL: 'https://mkt.indexadoc.com.br/api/',
 });
 
 // export const api = axios.create({
