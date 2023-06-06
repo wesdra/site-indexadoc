f63d4b12078f482bd2fdc0f6f312fd7d3f8b131c
wesdra <wesdralima@gmail.com>
2023-06-06 09:36:02 -0300

    atualização da apresentacao 04.svg

--- services/api.ts
+++ services/api.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://mkt.indexadoc.com.br/api/',
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImVxdWlwZSIsImlhdCI6MTUxNjIzOTAyMn0.7g6E13Hk4PC2Jp-vkuUBJmVJR1A6al4q6hRrgEHFfLE`
-    }
+  baseURL: "https://mkt.indexadoc.com.br/api/",
+  headers: {
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImVxdWlwZSIsImlhdCI6MTUxNjIzOTAyMn0.7g6E13Hk4PC2Jp-vkuUBJmVJR1A6al4q6hRrgEHFfLE`,
+  },
 });
-
 
 // export const api = axios.create({
 //   baseURL: 'http://localhost:17586/api/',
@@ -20,7 +19,7 @@ export const api = axios.create({
 // });
 
 export const apibase = axios.create({
-  baseURL: 'https://mkt.indexadoc.com.br/api/',
+  baseURL: "https://mkt.indexadoc.com.br/api/",
 });
 
 // export const api = axios.create({
@@ -29,4 +28,5 @@ export const apibase = axios.create({
 //     Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImVxdWlwZSIsImlhdCI6MTUxNjIzOTAyMn0.7g6E13Hk4PC2Jp-vkuUBJmVJR1A6al4q6hRrgEHFfLE`
 //   }
 // });
+
 
